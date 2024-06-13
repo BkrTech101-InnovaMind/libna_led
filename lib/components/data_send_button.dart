de8f4449341370002d1572daba111dc46d395442
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-13 10:50:52 +0300

    implement a send text color and bg color

--- lib/components/data_send_button.dart
+++ lib/components/data_send_button.dart
@@ -3,14 +3,30 @@ import 'package:flutter/material.dart';
 class DataSendButton extends StatelessWidget {
   final Map<String, dynamic> selectedButtonData;
   final VoidCallback onPressed;
+  final Color? color;
   const DataSendButton(
-      {super.key, required this.selectedButtonData, required this.onPressed});
+      {super.key,
+      required this.selectedButtonData,
+      required this.onPressed,
+      this.color});
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(20),
+        backgroundColor: color?.withOpacity(0.2),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+        ),
+        overlayColor: Colors.transparent,
+      ),
       onPressed: onPressed,
-      child: const Text("Send"),
+      child: Text("Send",
+          style: TextStyle(
+              fontSize: 20,
+              color: color ?? Colors.white,
+              fontWeight: FontWeight.w900)),
     );
   }
 }
