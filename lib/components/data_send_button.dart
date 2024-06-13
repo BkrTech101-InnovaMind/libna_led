f15a55adf5b21d0effdb5dff9792d1f49e7e5f93
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-13 11:08:22 +0300

    implement a color calculation

--- lib/components/data_send_button.dart
+++ lib/components/data_send_button.dart
@@ -4,6 +4,7 @@ class DataSendButton extends StatelessWidget {
   final Map<String, dynamic> selectedButtonData;
   final VoidCallback onPressed;
   final Color? color;
+
   const DataSendButton(
       {super.key,
       required this.selectedButtonData,
@@ -12,21 +13,35 @@ class DataSendButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color backgroundColor = color?.withOpacity(0.2) ?? Colors.transparent;
+    Color textColor = _getContrastingTextColor(backgroundColor);
+
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.all(20),
-        backgroundColor: color?.withOpacity(0.2),
+        backgroundColor: backgroundColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(100),
         ),
         overlayColor: Colors.transparent,
       ),
       onPressed: onPressed,
-      child: Text("Send",
-          style: TextStyle(
-              fontSize: 20,
-              color: color ?? Colors.white,
-              fontWeight: FontWeight.w900)),
+      child: Text(
+        "Send",
+        style: TextStyle(
+          fontSize: 20,
+          color: textColor,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
     );
+  }
+
+  Color _getContrastingTextColor(Color backgroundColor) {
+    double luminance = (0.299 * backgroundColor.red +
+            0.587 * backgroundColor.green +
+            0.114 * backgroundColor.blue) /
+        255;
+    return luminance > 0.5 ? Colors.black : Colors.white;
   }
 }
